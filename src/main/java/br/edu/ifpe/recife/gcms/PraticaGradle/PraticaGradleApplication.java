d06ad4cc00075df3e941dfa7f9b8024f484d90c5
Rodrigo <rodrigo.lopessouza@hotmail.com>
2024-08-14 19:58:55 -0300

    conserto pratica gradle

---
+++ src/main/java/br/edu/ifpe/recife/gcms/PraticaGradle/PraticaGradleApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.recife.gcms.PraticaGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaGradleApplication.class, args);
+	}
+
+}
